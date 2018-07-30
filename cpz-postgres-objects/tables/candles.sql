21893553d2ce8c381cdad78452b974edb2627eae
KoALa <makeag@gmail.com>
2018-07-30 17:05:34 +0300

    DB: correction

--- cpz-postgres-objects/tables/candles.sql
+++ cpz-postgres-objects/tables/candles.sql
@@ -1,3 +1,10 @@
+CREATE SEQUENCE candles_id_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
 CREATE TABLE candles
 (
     id bigint DEFAULT nextval('candles_id_seq'::regclass) PRIMARY KEY NOT NULL,
