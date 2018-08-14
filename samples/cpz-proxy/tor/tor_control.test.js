75beeff92b5b758a8e8ccd1a0b6e51db83acfb90
Dmitriy Shatalov <deman4ik@gmail.com>
2018-08-15 02:32:27 +0300

    clear

--- samples/cpz-proxy/tor/tor_control.test.js
+++
@@ -1,8 +0,0 @@
-const renewTorSession = require("./tor_controler");
-
-test("renewTorSession", async () => {
-  // VG
-  jest.setTimeout(12000);
-  const result = await renewTorSession();
-  expect(result).toBeDefined();
-});
