80422235df740d240b92d6939708424c28308a2b
Dmitriy Shatalov <deman4ik@gmail.com>
2018-09-10 16:01:39 +0300

    Updated services config

--- cpz-infrastructure/config.js
+++ cpz-infrastructure/config.js
@@ -19,6 +19,11 @@ const config = {
         name: "Stop any service",
         url: "/api/stop",
         method: "POST"
+      },
+      {
+        name: "Update any services configuration",
+        url: "/api/update",
+        method: "POST"
       }
     ],
     egIn: [
@@ -32,17 +37,25 @@ const config = {
           "CPZ.Tasks.MarketWatcher.Stopped",
           "CPZ.Tasks.MarketWatcher.Subscribed",
           "CPZ.Tasks.MarketWatcher.Unsubscribed",
+          "CPZ.Tasks.Candlebatcher.Started",
+          "CPZ.Tasks.Candlebatcher.Stopped",
+          "CPZ.Tasks.Candlebatcher.Updated",
           "CPZ.Tasks.Adviser.Started",
           "CPZ.Tasks.Adviser.Stopped",
+          "CPZ.Tasks.Adviser.Updated",
           "CPZ.Tasks.Trader.Started",
-          "CPZ.Tasks.Trader.Stopped"
+          "CPZ.Tasks.Trader.Stopped",
+          "CPZ.Tasks.Trader.Updated",
+          "CPZ.Tasks.Backtester.Started",
+          "CPZ.Tasks.Backtester.Stopped"
         ]
       }
     ],
     egOut: [
       {
         topic: "CPZ-LOG",
-        types: ["CPZ.Taskrunner.Log", "CPZ.Taskrunner.Error"]
+        types: ["CPZ.Taskrunner.Log", "CPZ.Taskrunner.Error"],
+        subject: "Realtime={Task Id}/Backtest={Task Id}.{Service Name}"
       },
       {
         topic: "CPZ-TASKS",
@@ -51,11 +64,20 @@ const config = {
           "CPZ.Tasks.MarketWatcher.Stop",
           "CPZ.Tasks.MarketWatcher.Subscribe",
           "CPZ.Tasks.MarketWatcher.Unsubscribe",
+          "CPZ.Tasks.Candlebatcher.Start",
+          "CPZ.Tasks.Candlebatcher.Stop",
+          "CPZ.Tasks.Candlebatcher.Update",
           "CPZ.Tasks.Adviser.Start",
           "CPZ.Tasks.Adviser.Stop",
+          "CPZ.Tasks.Adviser.Update",
           "CPZ.Tasks.Trader.Start",
-          "CPZ.Tasks.Trader.Stop"
-        ]
+          "CPZ.Tasks.Trader.Stop",
+          "CPZ.Tasks.Trader.Update",
+          "CPZ.Tasks.Backtester.Start",
+          "CPZ.Tasks.Backtester.Stop"
+        ],
+        subject:
+          "Realtime={Task Id}/Backtest={Task Id}.{Exchange}.{Base}.{Quote}.{Timeframe}.[Strategy]"
       }
     ],
     storageTables: ["Tasks"],
@@ -70,52 +92,9 @@ const config = {
       "EG_TASKS_KEY"
     ]
   },
-  importer: {
-    name: "CPZ-Importer",
-    description: "Request history data and saves to postgresql",
-    directory: "/cpz-importer",
-    localUrlPostfix: "/importer",
-    apiEndpoints: [
-      {
-        name: "Importer current status",
-        url: "/api/status",
-        method: "GET"
-      }
-    ],
-    egIn: [
-      {
-        name: "EventGrid Tasks Handler",
-        url: "/api/taskEvents",
-        method: "POST",
-        topic: "CPZ-TASKS",
-        types: ["CPZ.Tasks.Importer.Start", "CPZ.Tasks.Importer.Stop"]
-      }
-    ],
-    egOut: [
-      {
-        topic: "CPZ-LOG",
-        types: ["CPZ.Importer.Log", "CPZ.Importer.Error"]
-      },
-      {
-        topic: "CPZ-TASKS",
-        types: ["CPZ.Tasks.Importer.Started", "CPZ.Tasks.Importer.Stopped"]
-      }
-    ],
-    storageTables: ["Importers"],
-    environmentVariables: [
-      "AZ_STORAGE_CS",
-      "API_KEY",
-      "EG_TEST_ENDPOINT",
-      "EG_TEST_KEY",
-      "EG_LOG_ENDPOINT",
-      "EG_LOG_KEY",
-      "EG_TASKS_ENDPOINT",
-      "EG_TASKS_KEY"
-    ]
-  },
   marketWatcher: {
     name: "CPZ-MarketWatcher",
-    description: "Subscribe to market data, generate candles and ticks",
+    description: "Subscribe to realtime market data, generate ticks",
     directory: "/cpz-marketWatcher",
     localUrlPostfix: "/marketwatcher",
     apiEndpoints: [
@@ -136,28 +115,34 @@ const config = {
           "CPZ.Tasks.MarketWatcher.Stop",
           "CPZ.Tasks.MarketWatcher.Subscribe",
           "CPZ.Tasks.MarketWatcher.Unsubscribe"
-        ]
+        ],
+        subject:
+          "Realtime={Task Id}/Backtest={Task Id}.{Exchange}.{Base}.{Quote}"
       }
     ],
     egOut: [
       {
         topic: "CPZ-LOG",
-        types: ["CPZ.MarketWatcher.Log", "CPZ.MarketWatcher.Error"]
+        types: ["CPZ.MarketWatcher.Log", "CPZ.MarketWatcher.Error"],
+        subject:
+          "Realtime={Task Id}/Backtest={Task Id}.{Exchange}.{Base}.{Quote}"
       },
       {
         topic: "CPZ-TASKS",
         types: [
           "CPZ.Tasks.MarketWatcher.Started",
-          "CPZ.Tasks.MarketWatcher.Stopped"
-        ]
-      },
-      {
-        topic: "CPZ-CANDLES",
-        types: ["CPZ.Candles.NewCandle"]
+          "CPZ.Tasks.MarketWatcher.Stopped",
+          "CPZ.Tasks.MarketWatcher.Subscribed",
+          "CPZ.Tasks.MarketWatcher.Unsubscribed"
+        ],
+        subject:
+          "Realtime={Task Id}/Backtest={Task Id}.{Exchange}.{Base}.{Quote}.{Timeframe}"
       },
       {
         topic: "CPZ-TICKS",
-        types: ["CPZ.Ticks.NewTick"]
+        types: ["CPZ.Ticks.NewTick"],
+        subject:
+          "Realtime={Task Id}/Backtest={Task Id}.{Exchange}.{Base}.{Quote}.{Timeframe}"
       }
     ],
     storageTables: ["MarketWatchers"],
@@ -170,35 +155,62 @@ const config = {
       "EG_LOG_KEY",
       "EG_TASKS_ENDPOINT",
       "EG_TASKS_KEY",
-      "EG_CANDLES_ENDPOINT",
-      "EG_CANDLES_KEY",
       "EG_TICKS_ENDPOINT",
-      "EG_TICKS_KEY"
+      "EG_TICKS_KEY",
+      "PROXY_ENDPOINT"
     ]
   },
   candlebatcher: {
     name: "CPZ-Candlebatcher",
     description:
-      "Handle candles, save them to postgre, generate candles in different timeframes",
+      "Request history candles, save them to postgresql, generate candles in different timeframes",
     directory: "/cpz-candlebatcher",
     localUrlPostfix: "/candlebatcher",
     egIn: [
       {
-        name: "EventGrid Candles Handler",
-        url: "/api/candleEvents",
+        name: "EventGrid Tasks Handler",
+        url: "/api/taskEvents",
         method: "POST",
-        topic: "CPZ-CANDLES",
-        types: ["CPZ.Candles.NewCandle"]
+        topic: "CPZ-TASKS",
+        types: [
+          "CPZ.Tasks.Candlebatcher.Start",
+          "CPZ.Tasks.Candlebatcher.Stop",
+          "CPZ.Tasks.Candlebatcher.Update",
+          "CPZ.Tasks.Candlebatcher.StartImport",
+          "CPZ.Tasks.Candlebatcher.StopImport",
+          "CPZ.Tasks.Candlebatcher.StartBacktest",
+          "CPZ.Tasks.Candlebatcher.StopBacktest"
+        ],
+        subject:
+          "Realtime={Task Id}/Backtest={Task Id}.{Exchange}.{Base}.{Quote}"
       }
     ],
     egOut: [
       {
         topic: "CPZ-LOG",
-        types: ["CPZ.Candlebatcher.Log", "CPZ.Candlebatcher.Error"]
+        types: ["CPZ.Candlebatcher.Log", "CPZ.Candlebatcher.Error"],
+        subject:
+          "Realtime={Task Id}/Backtest={Task Id}.{Exchange}.{Base}.{Quote}"
+      },
+      {
+        topic: "CPZ-TASKS",
+        types: [
+          "CPZ.Tasks.Candlebatcher.Started",
+          "CPZ.Tasks.Candlebatcher.Stopped",
+          "CPZ.Tasks.Candlebatcher.Updated",
+          "CPZ.Tasks.Candlebatcher.StartedImport",
+          "CPZ.Tasks.Candlebatcher.StoppedImport",
+          "CPZ.Tasks.Candlebatcher.StartedBacktest",
+          "CPZ.Tasks.Candlebatcher.StoppedBacktest"
+        ],
+        subject:
+          "Realtime={Task Id}/Backtest={Task Id}.{Exchange}.{Base}.{Quote}"
       },
       {
         topic: "CPZ-CANDLES",
-        types: ["CPZ.Candles.NewCandle", "CPZ.Candles.CandleSaved"]
+        types: ["CPZ.Tasks.Candles.NewCandle"],
+        subject:
+          "Realtime={Task Id}/Backtest={Task Id}.{Exchange}.{Base}.{Quote}"
       }
     ],
     environmentVariables: [
@@ -211,12 +223,13 @@ const config = {
       "EG_CANDLES_ENDPOINT",
       "EG_CANDLES_KEY",
       "DB_API_ENDPOINT",
-      "DB_API_KEY"
+      "DB_API_KEY",
+      "PROXY_ENDPOINT"
     ]
   },
   backtester: {
     name: "CPZ-Backtester",
-    description: "Run backtests, generate candles",
+    description: "Run backtests, saves results",
     directory: "/cpz-backtester",
     localUrlPostfix: "/backtester",
     egIn: [
@@ -225,45 +238,53 @@ const config = {
         url: "/api/taskEvents",
         method: "POST",
         topic: "CPZ-TASKS",
-        types: ["CPZ.Tasks.Backtester.Start", "CPZ.Tasks.Backtester.Stop"]
+        types: ["CPZ.Tasks.Backtester.Start", "CPZ.Tasks.Backtester.Stop"],
+        subject:
+          "Backtest={Task Id}.{Exchange}.{Base}.{Quote}.{Timeframe}.{Strategy}"
       },
       {
         name: "EventGrid Candles Handler",
         url: "/api/candleEvents",
         method: "POST",
         topic: "CPZ-CANDLES",
-        types: ["CPZ.Candles.Handled"]
+        types: ["CPZ.Candles.NewCandle", "CPZ.Candles.Handled"],
+        subject:
+          "Backtest={Task Id}.{Exchange}.{Base}.{Quote}.{Timeframe}.{Strategy}"
       },
       {
         name: "EventGrid Signals Handler",
         url: "/api/signalEvents",
         method: "POST",
         topic: "CPZ-SIGNALS",
-        types: ["CPZ.Signals.NewSignal", "CPZ.Signals.Handled"]
+        types: ["CPZ.Signals.NewSignal", "CPZ.Signals.Handled"],
+        subject:
+          "Backtest={Task Id}.{Exchange}.{Base}.{Quote}.{Timeframe}.{Strategy}"
       },
       {
         name: "EventGrid Orders Handler",
         url: "/api/orderEvents",
         method: "POST",
         topic: "CPZ-ORDERS",
-        types: ["CPZ.Orders.NewOpenOrder", "CPZ.Orders.NewCloseOrder"]
+        types: ["CPZ.Orders.NewOpenOrder", "CPZ.Orders.NewCloseOrder"],
+        subject:
+          "Backtest={Task Id}.{Exchange}.{Base}.{Quote}.{Timeframe}.{Strategy}"
       }
     ],
     egOut: [
       {
         topic: "CPZ-LOG",
-        types: ["CPZ.Backtester.Log", "CPZ.Backtester.Error"]
+        types: ["CPZ.Backtester.Log", "CPZ.Backtester.Error"],
+        subject:
+          "Backtest={Task Id}.{Exchange}.{Base}.{Quote}.{Timeframe}.{Strategy}"
       },
       {
         topic: "CPZ-TASKS",
-        types: ["CPZ.Tasks.Backtester.Started", "CPZ.Tasks.Backtester.Stopped"]
-      },
-      {
-        topic: "CPZ-CANDLES",
-        types: ["CPZ.Candles.NewCandle"]
+        types: ["CPZ.Tasks.Backtester.Started", "CPZ.Tasks.Backtester.Stopped"],
+        subject:
+          "Backtest={Task Id}.{Exchange}.{Base}.{Quote}.{Timeframe}.{Strategy}"
       }
     ],
-    storageTables: ["Backtesters"],
+    storageTables: ["Backtests", "BacktestItems"],
     environmentVariables: [
       "AZ_STORAGE_CS",
       "API_KEY",
@@ -272,9 +293,7 @@ const config = {
       "EG_LOG_ENDPOINT",
       "EG_LOG_KEY",
       "EG_TASKS_ENDPOINT",
-      "EG_TASKS_KEY",
-      "EG_CANDLES_ENDPOINT",
-      "EG_CANDLES_KEY"
+      "EG_TASKS_KEY"
     ]
   },
   adviser: {
@@ -288,32 +307,52 @@ const config = {
         url: "/api/taskEvents",
         method: "POST",
         topic: "CPZ-TASKS",
-        types: ["CPZ.Tasks.Adviser.Start", "CPZ.Tasks.Adviser.Stop"]
+        types: [
+          "CPZ.Tasks.Adviser.Start",
+          "CPZ.Tasks.Adviser.Stop",
+          "CPZ.Tasks.Adviser.Update"
+        ],
+        subject:
+          "Realtime={Task Id}/Backtest={Task Id}.{Exchange}.{Base}.{Quote}.{Timeframe}.{Strategy}"
       },
       {
         name: "EventGrid Candles Handler",
         url: "/api/candleEvents",
         method: "POST",
         topic: "CPZ-CANDLES",
-        types: ["CPZ.Candles.NewCandle"]
+        types: ["CPZ.Candles.NewCandle"],
+        subject:
+          "Realtime={Task Id}/Backtest={Task Id}.{Exchange}.{Base}.{Quote}.{Timeframe}.[Strategy]"
       }
     ],
     egOut: [
       {
         topic: "CPZ-LOG",
-        types: ["CPZ.Adviser.Log", "CPZ.Adviser.Error"]
+        types: ["CPZ.Adviser.Log", "CPZ.Adviser.Error"],
+        subject:
+          "Realtime={Task Id}/Backtest={Task Id}.{Exchange}.{Base}.{Quote}.{Timeframe}.{Strategy}"
       },
       {
         topic: "CPZ-TASKS",
-        types: ["CPZ.Tasks.Adviser.Started", "CPZ.Tasks.Adviser.Stopped"]
+        types: [
+          "CPZ.Tasks.Adviser.Started",
+          "CPZ.Tasks.Adviser.Stopped",
+          "CPZ.Tasks.Adviser.Updated"
+        ],
+        subject:
+          "Realtime={Task Id}/Backtest={Task Id}.{Exchange}.{Base}.{Quote}.{Timeframe}.{Strategy}"
       },
       {
         topic: "CPZ-CANDLES",
-        types: ["CPZ.Candles.Handled"]
+        types: ["CPZ.Candles.Handled"],
+        subject:
+          "Realtime={Task Id}/Backtest={Task Id}.{Exchange}.{Base}.{Quote}.{Timeframe}.{Strategy}"
       },
       {
         topic: "CPZ-SIGNALS",
-        types: ["CPZ.Signals.NewSignal"]
+        types: ["CPZ.Signals.NewSignal"],
+        subject:
+          "Realtime={Task Id}/Backtest={Task Id}.{Exchange}.{Base}.{Quote}.{Timeframe}.{Strategy}"
       }
     ],
     storageTables: ["Advisers"],
@@ -344,32 +383,52 @@ const config = {
         url: "/api/taskEvents",
         method: "POST",
         topic: "CPZ-TASKS",
-        types: ["CPZ.Tasks.Trader.Start", "CPZ.Tasks.Trader.Stop"]
+        types: [
+          "CPZ.Tasks.Trader.Start",
+          "CPZ.Tasks.Trader.Stop",
+          "CPZ.Tasks.Trader.Update"
+        ],
+        subject:
+          "Realtime={Task Id}/Backtest={Task Id}.{Exchange}.{Base}.{Quote}.{Timeframe}.{Strategy}"
       },
       {
         name: "EventGrid Signals Handler",
         url: "/api/signalEvents",
         method: "POST",
         topic: "CPZ-SIGNALS",
-        types: ["CPZ.Signals.NewSignal"]
+        types: ["CPZ.Signals.NewSignal"],
+        subject:
+          "Realtime={Task Id}/Backtest={Task Id}.{Exchange}.{Base}.{Quote}.{Timeframe}.{Strategy}"
       }
     ],
     egOut: [
       {
         topic: "CPZ-LOG",
-        types: ["CPZ.Trader.Log", "CPZ.Trader.Error"]
+        types: ["CPZ.Trader.Log", "CPZ.Trader.Error"],
+        subject:
+          "Realtime={Task Id}/Backtest={Task Id}.{Exchange}.{Base}.{Quote}.{Timeframe}.{Strategy}"
       },
       {
         topic: "CPZ-TASKS",
-        types: ["CPZ.Tasks.Trader.Started", "CPZ.Tasks.Trader.Stopped"]
+        types: [
+          "CPZ.Tasks.Trader.Started",
+          "CPZ.Tasks.Trader.Stopped",
+          "CPZ.Tasks.Trader.Updated"
+        ],
+        subject:
+          "Realtime={Task Id}/Backtest={Task Id}.{Exchange}.{Base}.{Quote}.{Timeframe}.{Strategy}"
       },
       {
         topic: "CPZ-SIGNALS",
-        types: ["CPZ.Signals.Handled"]
+        types: ["CPZ.Signals.Handled"],
+        subject:
+          "Realtime={Task Id}/Backtest={Task Id}.{Exchange}.{Base}.{Quote}.{Timeframe}.{Strategy}"
       },
       {
         topic: "CPZ-ORDERS",
-        types: ["CPZ.Orders.NewOpenOrder", "CPZ.Orders.NewCloseOrder"]
+        types: ["CPZ.Orders.NewOpenOrder", "CPZ.Orders.NewCloseOrder"],
+        subject:
+          "Realtime={Task Id}/Backtest={Task Id}.{Exchange}.{Base}.{Quote}.{Timeframe}.{Strategy}"
       }
     ],
     storageTables: ["Traders"],
@@ -387,7 +446,38 @@ const config = {
       "EG_SIGNALS_KEY",
       "EG_ORDERS_ENDPOINT",
       "EG_ORDERS_KEY",
-      "CCXT_ENDPOINT"
+      "CCXT_ENDPOINT",
+      "KV_CLIENT_ID",
+      "KV_CLIENT_SECRET"
+    ]
+  },
+  ccxt: {
+    name: "CPZ-Connector-CCXT",
+    description: "HTTP API to send authenticated requests to exchanges",
+    directory: "/cpz-connector-ccxt",
+    localUrlPostfix: "/ccxt",
+    apiEndpoints: [
+      {
+        name: "Create new order",
+        url: "/api/setOrder",
+        method: "POST"
+      },
+      {
+        name: "Check order",
+        url: "/api/checkOrder",
+        method: "POST"
+      },
+      {
+        name: "Cancele order",
+        url: "/api/cancelOrder",
+        method: "POST"
+      }
+    ],
+    environmentVariables: [
+      "API_KEY",
+      "KV_CLIENT_ID",
+      "KV_CLIENT_SECRET",
+      "PROXY_ENDPOINT"
     ]
   },
   postgraphile: {
