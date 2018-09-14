6e26a634009f8b74cedcad5f6c31c1a9104be62a
Dmitriy Shatalov <deman4ik@gmail.com>
2018-09-14 18:53:17 +0300

    remove cpz-positions-controller

--- cpz-positions-controller/utils/constants.js
+++
@@ -1,9 +0,0 @@
-const SUB_VALIDATION_EVENT = "Microsoft.EventGrid.SubscriptionValidationEvent";
-const NEW_TICK_EVENT = "CPZ.Ticks.NewTick";
-const SIGNALS_NEW_SIGNAL_EVENT = "CPZ.Signals.NewSignal";
-
-module.exports = {
-  SUB_VALIDATION_EVENT,
-  NEW_TICK_EVENT,
-  SIGNALS_NEW_SIGNAL_EVENT
-};
