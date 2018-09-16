bc54104d7bed0ab0daa94022bec5983076c7e14c
Dmitriy Shatalov <deman4ik@gmail.com>
2018-09-16 14:12:27 +0300

    Importer moved to Candlebatcher

--- cpz-importer/utils/constants.js
+++
@@ -1,9 +0,0 @@
-const SUB_VALIDATION_EVENT = "Microsoft.EventGrid.SubscriptionValidationEvent";
-const START_EVENT = "CPZ.Tasks.Importer.Start";
-const STOP_EVENT = "CPZ.Tasks.Importer.STOP";
-
-module.exports = {
-  SUB_VALIDATION_EVENT,
-  START_EVENT,
-  STOP_EVENT
-};
