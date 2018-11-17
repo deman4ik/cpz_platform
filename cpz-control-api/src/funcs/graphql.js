115927c277a8358759990f3f90a43446dbd2f685
Dmitriy Shatalov <deman4ik@gmail.com>
2018-11-17 21:40:57 +0300

    cpz-control-api project init

---
+++ cpz-control-api/src/funcs/graphql.js
@@ -0,0 +1,19 @@
+import { gql, ApolloServer } from "apollo-server-azure-functions";
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// A map of functions which return data for the schema.
+const resolvers = {
+  Query: {
+    hello: () => "world"
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+export default server.createHandler();
