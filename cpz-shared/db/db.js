f819307c2b1d0241b763106317c7def555413fdd
Dmitriy Shatalov <deman4ik@gmail.com>
2018-12-06 22:37:08 +0300

    temp db api

--- cpz-shared/db/db.js
+++ cpz-shared/db/db.js
@@ -6,6 +6,7 @@ import { saveSignal } from "./signals";
 import { saveOrder } from "./orders";
 import { saveLogEvent } from "./logsEvents";
 import { saveErrorEvent } from "./errorsEvents";
+import { getUserRobot, saveUserRobot } from "./userRobots";
 
 class DataBaseAPI {
   constructor() {
@@ -19,6 +20,9 @@ class DataBaseAPI {
     this.saveCandles = saveCandles;
     this.getCandles = getCandles;
     this.countCandles = countCandles;
+
+    this.getUserRobot = getUserRobot;
+    this.saveUserRobot = saveUserRobot;
 
     this.saveTasksEvent = saveTasksEvent;
     this.saveSignal = saveSignal;
---
+++ cpz-shared/db/userRobots.js
@@ -0,0 +1,9 @@
+async function getUserRobot({ id }) {
+  // ! TODO
+}
+
+async function saveUserRobot(data) {
+  // ! TODO
+}
+
+export { getUserRobot, saveUserRobot };
