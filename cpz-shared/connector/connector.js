be3d1dc20dea7f518afc594e25369b5a3fe0a50e
Dmitriy Shatalov <deman4ik@gmail.com>
2019-01-25 21:51:40 +0300

    connector client headers fix

--- cpz-shared/connector/connector.js
+++ cpz-shared/connector/connector.js
@@ -3,7 +3,7 @@ import { GraphQLClient } from "graphql-request";
 const { CONNECTOR_API_ENDPOINT, CONNECTOR_API_KEY } = process.env;
 const client = new GraphQLClient(CONNECTOR_API_ENDPOINT, {
   headers: {
-    "API-Key": `${CONNECTOR_API_KEY}`
+    "api-key": `${CONNECTOR_API_KEY}`
     // TODO: Authorization
   }
 });
