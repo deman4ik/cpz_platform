47b44a686d37fa0d1ffdb9011d0d04f3d2d0a3f3
Dmitriy Shatalov <deman4ik@gmail.com>
2019-02-08 17:17:45 +0300

    tests run configuration

--- cpz-adviser/jest.config.js
+++ cpz-adviser/jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   verbose: true,
-  reporters: ["default", "jest-junit"]
+  reporters: ["default", "jest-junit"],
+  collectCoverage: true
 };
--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   verbose: true,
   reporters: ["default", "jest-junit"],
+  collectCoverage: true,
   projects: ["<rootDir>/cpz-adviser", "<rootDir>/cpz-shared"],
   testEnvironment: "node"
 };
