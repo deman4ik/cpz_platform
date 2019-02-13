410a2d707e8d07e1c29dc551b805b6feb95c10ab
Dmitriy Shatalov <deman4ik@gmail.com>
2019-02-13 23:16:13 +0300

    jest cobertura coverage reporter

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   verbose: true,
   reporters: ["default", "jest-junit"],
   collectCoverage: true,
+  coverageReporters: ["text", "cobertura"],
   projects: ["<rootDir>/cpz-adviser", "<rootDir>/cpz-shared"],
   testEnvironment: "node"
 };
