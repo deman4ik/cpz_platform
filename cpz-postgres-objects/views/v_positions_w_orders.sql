710bcf417736833dad1f051c750873c51e39b452
KoALa <makeag@gmail.com>
2019-02-20 22:14:48 +0300

    DB: added order ex num

--- cpz-postgres-objects/views/v_positions_w_orders.sql
+++ cpz-postgres-objects/views/v_positions_w_orders.sql
@@ -29,7 +29,9 @@ SELECT p.id,
        b.ended_at,
        b.note,
        oo.status as o_status_open,  oo.created_at as o_created_at_open,  oo.order_type as o_type_open,  oo.order_price as o_price_open,  oo.candle_timestamp as o_candle_timestamp_open,
-       oc.status as o_status_close, oc.created_at as o_created_at_close, oc.order_type as o_type_close, oc.order_price as o_price_close, oc.candle_timestamp as o_candle_timestamp_close
+       oo.order_ex_num as o_ex_num_open,
+       oc.status as o_status_close, oc.created_at as o_created_at_close, oc.order_type as o_type_close, oc.order_price as o_price_close, oc.candle_timestamp as o_candle_timestamp_close,
+       oc.order_ex_num as o_ex_num_close
 FROM positions p
 left join backtest b on (p.backtest_id = b.id)
 left join orders oo on (p.id = oo.position_id and oo.action in ('short','long'))
