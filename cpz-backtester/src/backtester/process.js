75b318d8ed3a452e8ceda9a89996e5ef76a5b1aa
Dmitriy Shatalov <deman4ik@gmail.com>
2019-02-21 23:03:16 +0300

    backtester catch init errors

--- cpz-backtester/src/backtester/process.js
+++ cpz-backtester/src/backtester/process.js
@@ -4,11 +4,18 @@ import Backtester from "./backtester";
 process.on("message", async m => {
   const eventData = JSON.parse(m);
   if (eventData.type === "start") {
-    const backtester = new Backtester(eventData.state);
-    await backtester.execute();
+    try {
+      const backtester = new Backtester(eventData.state);
+      await backtester.execute();
+    } catch (error) {
+      process.send([
+        `Backtester ${eventData.state.taskId} initialization error`,
+        error.message
+      ]);
+    }
     process.exit(0);
   } else if (eventData.type === "stop") {
-    process.send([`Backtester ${eventData.taskId} stopped!`]);
+    process.send([`Backtester ${eventData.state.taskId} stopped!`]);
     process.exit(0);
   } else {
     process.send(["Unknown child process event type"]);
