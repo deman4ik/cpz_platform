1b24a63099772e471661567953451e09f8b44e6e
hyrdbyrd <IRuss23@yandex.ru>
2019-02-23 19:14:27 +0300

    tests: add tests for cpz-events-logger

--- cpz-events-logger/jest.config.js
+++ cpz-events-logger/jest.config.js
@@ -1,4 +1,24 @@
 module.exports = {
   verbose: true,
-  reporters: ["default", "jest-junit"]
+  reporters: ["default", "jest-junit"],
+  moduleFileExtensions: ["js"],
+  testEnvironment: "node",
+  clearMocks: true,
+  moduleNameMapper: {
+    cpzConfig: "<rootDir>../cpz-shared/config",
+    cpzEnv: "<rootDir>../cpz-shared/config/environment",
+    cpzDefaults: "<rootDir>../cpz-shared/config/defaults",
+    cpzEventTypes: "<rootDir>../cpz-shared/config/events/types",
+    cpzServices: "<rootDir>../cpz-shared/config/services",
+    cpzState: "<rootDir>../cpz-shared/config/state",
+    cpzStorageTables: "<rootDir>../cpz-shared/config/storageTables",
+    "^cpzStorage.(.*)$": "<rootDir>../cpz-shared/tableStorage/$1",
+    cpzQueuesList: "<rootDir>../cpz-shared/config/queues",
+    cpzQueue: "<rootDir>../cpz-shared/queueStorage",
+    cpzEvents: "<rootDir>../cpz-shared/eventgrid",
+    "^cpzUtils.(.*)$": "<rootDir>../cpz-shared/utils/$1",
+    cpzDayjs: "<rootDir>../cpz-shared/utils/lib/dayjs",
+    cpzDB: "<rootDir>../cpz-shared/db",
+    cpzConnector: "<rootDir>../cpz-shared/connector"
+  }
 };
