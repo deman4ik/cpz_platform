227f120db0e9eee8dcc27cbe56e1d9b4c6776012
Dmitriy Shatalov <deman4ik@gmail.com>
2019-02-26 23:44:25 +0300

    logging and analytics

---
+++ cpz-shared/tests/utils/log.test.js
@@ -0,0 +1,48 @@
+import Log from "../../utils/log";
+import { contextMock } from "../../../tests/helpers";
+
+describe("Log helper should log data", () => {
+  test("Should log info to console", () => {
+    const spy = jest.spyOn(Log, "_logInfo");
+    Log.info("info");
+    expect(spy).toHaveBeenCalled();
+  });
+  test("Should log warn to console", () => {
+    const spy = jest.spyOn(Log, "_logWarn");
+    Log.warn("warn");
+    expect(spy).toHaveBeenCalled();
+  });
+  test("Should log error to console", () => {
+    const spy = jest.spyOn(Log, "_logError");
+    Log.error("error");
+    expect(spy).toHaveBeenCalled();
+  });
+  test("Should throw error for invalid context", () => {
+    const context = {};
+    expect(() => {
+      Log.addContext(context);
+    }).toThrow();
+  });
+  test("Should log info to context.info", () => {
+    const context = contextMock();
+    const spy = jest.spyOn(context.log, "info");
+    Log.addContext(context);
+    Log.info("info");
+    expect(spy).toHaveBeenCalled();
+  });
+  test("Should log warn to context.warn", () => {
+    const context = contextMock();
+    const spy = jest.spyOn(context.log, "warn");
+    Log.addContext(context);
+    Log.warn("warn");
+    expect(spy).toHaveBeenCalled();
+  });
+  test("Should log error to context.error", () => {
+    const context = contextMock();
+    const spy = jest.spyOn(context.log, "error");
+    Log.addContext(context);
+    Log.error("error");
+    expect(spy).toHaveBeenCalled();
+  });
+  // TODO: AppInsights related tests
+});
---
+++ cpz-shared/utils/log.js
@@ -0,0 +1,165 @@
+import VError from "verror";
+import appInsights from "applicationinsights";
+
+/**
+ * Логирование и сбор аналитики
+ *
+ * @class Log
+ */
+class Log {
+  constructor() {
+    this._logInfo = console.log;
+    this._logWarn = console.warn;
+    this._logError = console.error;
+    this._appInstightsEnabled = !!process.env.APPINSIGHTS_INSTRUMENTATIONKEY;
+    if (this._appInstightsEnabled) {
+      appInsights
+        .setup()
+        .setAutoDependencyCorrelation(true)
+        .setAutoCollectRequests(true)
+        .setAutoCollectPerformance(true)
+        .setAutoCollectExceptions(true)
+        .setAutoCollectDependencies(true)
+        .setAutoCollectConsole(true, true)
+        .setUseDiskRetryCaching(true);
+
+      appInsights.start();
+    }
+  }
+
+  /**
+   * Установка имени сервиса для службы аналитики
+   *
+   * @param {string} serviceName
+   * @memberof Log
+   */
+  setAppInsightsService(serviceName) {
+    if (this._appInstightsEnabled) {
+      appInsights.defaultClient.context.tags[
+        appInsights.defaultClient.context.keys.cloudRole
+      ] = serviceName;
+    }
+  }
+
+  /**
+   * Установка текущего контекста запроса
+   * для рантайма Azure Functions
+   *
+   * @param {Object} context
+   * @param {function} context.log
+   * @param {function} context.log.info
+   * @param {function} context.log.warn
+   * @param {function} context.log.error
+   * @memberof Log
+   */
+  addContext({ log }) {
+    if (!log || !log.info || !log.warn || !log.error)
+      throw new VError(
+        { name: "LogError" },
+        "Failed to add context to Log Instance"
+      );
+    this._logInfo = log.info;
+    this._logWarn = log.warn;
+    this._logError = log.error;
+  }
+
+  /**
+   * Логирование в консоль уровня info
+   *
+   * @param {*} args
+   * @memberof Log
+   */
+  info(...args) {
+    this._logInfo(...args);
+  }
+
+  /**
+   * Логирование в консоль уровня warn
+   *
+   * @param {*} args
+   * @memberof Log
+   */
+  warn(...args) {
+    this._logWarn(...args);
+  }
+
+  /**
+   * Логирование в консоль уровня error
+   *
+   * @param {*} args
+   * @memberof Log
+   */
+  error(...args) {
+    this._logError(...args);
+  }
+
+  /**
+   * Отправка события в службу аналитики
+   *
+   * @param {Object} eventData
+   * @memberof Log
+   */
+  event(eventData) {
+    if (this._appInstightsEnabled) {
+      if (eventData) {
+        // TODO: Parse base Event Types from config
+        const name = eventData.name || "UnknownEvent"; // TODO: Create constant
+        appInsights.defaultClient.trackEvent({ name, properties: eventData });
+      }
+    }
+  }
+
+  /**
+   * Логирование критических ошибок и отправка в службу аналитики
+   *
+   * @param {*} errorData
+   * @memberof Log
+   */
+  exception(errorData) {
+    if (this._appInstightsEnabled) {
+      if (errorData) {
+        // TODO: Check errorData type - Error or VError
+        const errorString = JSON.stringify(errorData); // TODO: Better VError stringify
+        this._logError(errorString);
+        appInsights.defaultClient.trackException({
+          exception: errorData
+        });
+      }
+    }
+  }
+
+  /**
+   * Отправка метрики в службу аналитики
+   *
+   * @param {Object} metric
+   * @param {string} metric.name
+   * @param {string} metric.value
+   * @memberof Log
+   */
+  metric({ name, value }) {
+    if (this._appInstightsEnabled) {
+      if (name && value) {
+        appInsights.defaultClient.trackMetric({ name, value });
+      }
+    }
+  }
+
+  /**
+   * Отправка информации о запросах к внешним сервисам в службу аналитики
+   *
+   * @param {Object} data
+   * @memberof Log
+   * @example data
+   * {target:"http://dbname", name:"select customers proc", data:"SELECT * FROM Customers", duration:231, resultCode:0, success: true, dependencyTypeName: "ZSQL"}
+   */
+  dependency(data) {
+    if (this._appInstightsEnabled) {
+      if (data && data.target && data.name && data.data)
+        appInsights.defaultClient.trackDependency(data);
+    }
+  }
+}
+
+const log = new Log();
+
+export default log;
