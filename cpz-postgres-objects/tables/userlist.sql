7f833a8d74f1a75edfe1bf203f00df1f5a5e6959
Dmitriy Shatalov <deman4ik@gmail.com>
2019-03-25 15:09:35 +0000

    Updated userlist.sql

--- cpz-postgres-objects/tables/userlist.sql
+++ cpz-postgres-objects/tables/userlist.sql
@@ -2,7 +2,7 @@ create table userlist
 (
   id uuid not null constraint c_userlist_pk primary key,
   email varchar(160) not null,
-  first_name varchar(160) not null,
+  first_name varchar(160),
   last_name varchar(160),
   reg_date date default CURRENT_DATE not null,
   status integer default 0 not null,
