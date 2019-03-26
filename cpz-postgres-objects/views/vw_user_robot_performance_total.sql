0f0080e10f32b747495f2e8e6d817e29bf451f7c
KoALa <makeag@gmail.com>
2019-03-26 08:53:13 +0300

    DB: revert changes performance

--- cpz-postgres-objects/views/vw_user_robot_performance_total.sql
+++ cpz-postgres-objects/views/vw_user_robot_performance_total.sql
@@ -2,12 +2,14 @@ drop view vw_user_robot_performance_total;
 
 create view vw_user_robot_performance_total as
 select
-p.user_id as uiduser_id, exit_date::date as dDATE, sum(p.profit) as nprofit
-from positions p, user_robot u
-where u.robot_id = p.robot_id and u.user_id = p.user_id
+p.user_id as uiduser_id, exit_date::date as dDATE, sum(p.profit*r.ncurrate) as nprofit
+from positions p, user_robot u, user_robot uu,
+     (select 4012 as nCURRATE, '$' as sCURCODE) r
+where (u.robot_id = p.robot_id and u.user_id = p.user_id or
+       u.linked_user_robot_id = u.id and uu.robot_id = p.robot_id and uu.user_id = p.user_id)
   and p.profit is not null
   --and p.backtest_id is null
-  and run_mode != 'backtest'
+  and p.run_mode != 'backtest'
 group by p.user_id, exit_date::date;
 
 alter table vw_user_robot_performance_total
