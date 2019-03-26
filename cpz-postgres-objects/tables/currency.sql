411fe5617e57d6a40dd95ad3fc6981717fb26650
KoALa <makeag@gmail.com>
2019-03-26 08:50:35 +0300

    DB: currency and performance

--- cpz-postgres-objects/tables/currency.sql
+++ cpz-postgres-objects/tables/currency.sql
@@ -4,11 +4,15 @@ create table currency
       constraint c_currency_pk primary key,
     name         varchar(160) not null,
     country_name varchar(160),
-  	enabled 		 integer not null default 20
+  	enabled 		 integer not null default 20,
+  	enabled_for_equiv_display integer not null default 0;
 );
 alter table currency
 	add constraint c_currency_enabled_chk
 		check (enabled in (0,10,20));
+alter table currency
+	add constraint c_currency_enabled_for_equiv_display_chk
+		check (enabled_for_equiv_display in (0,1));		
 		
 comment on column currency.enabled is '
 0 - disabled
