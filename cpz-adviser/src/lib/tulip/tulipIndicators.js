7505d6718871494f53915ebc9f4103dc4237d7a8
Dmitriy Shatalov <deman4ik@gmail.com>
2019-05-07 21:48:31 +0300

    feat: tulip candlesLength options

--- cpz-adviser/src/lib/tulip/tulipIndicators.js
+++ cpz-adviser/src/lib/tulip/tulipIndicators.js
@@ -4,12 +4,15 @@ import tulip from "./create";
 class Tulip extends BaseIndicator {
   constructor(state) {
     super(state);
-
     this.calculate = tulip[state.indicatorName].create(state.options);
   }
 
   async calc() {
-    const result = await this.calculate(this.candlesProps);
+    const { candlesLength } = this.options;
+    const candlesProps = this.prepareCandles(
+      this.candles.slice(-candlesLength)
+    );
+    const result = await this.calculate(candlesProps);
     const resultKeys = Object.keys(result);
     if (resultKeys.length > 0) {
       resultKeys.forEach(key => {
