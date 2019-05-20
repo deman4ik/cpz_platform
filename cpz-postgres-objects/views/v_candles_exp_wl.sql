8ad2d04b35892fe4edb3252683e398ddb4f931f2
KoALa <makeag@gmail.com>
2019-05-20 11:48:40 +0300

    DB: exp WL view

--- cpz-postgres-objects/views/v_candles_exp_wl.sql
+++ cpz-postgres-objects/views/v_candles_exp_wl.sql
@@ -1,12 +1,12 @@
 drop view v_candles_exp_wl;
 
-create view v_candles_exp_wl as
+create or replace view v_candles_exp_wl as
 select
 '<TICKER>' AS TICKER,'<PER>' as PER,'<DATE>' as "DATE", '<TIME>' as "TIME",'<OPEN>' as OPEN,'<HIGH>' as HIGH,'<LOW>' as LOW,'<CLOSE>' as CLOSE,'<VOL>' as VOL
 union all
 (select
   /*to change dataset name and timeframe !!*/
-  'KRAKEN.BTC-USD', 1::varchar, 
+  'KRAKEN.BTC-USD', 60::varchar,
   
   to_char(t.timestamp, 'YYYYMMDD'), to_char(t.timestamp, 'HH24MISS'), open::varchar, high::varchar, low::varchar, close::varchar, volume::varchar
   /*to change timeframe table*/
@@ -16,7 +16,7 @@ union all
    asset = 'BTC' 
    and currency = 'USD' 
    and exchange = 'kraken'
-   and t.timestamp >= '2019-01-03 22:30:00.000000' 
-   and t.timestamp < '2019-01-04 05:00:00.000000'
+   and t.timestamp >= '2019-02-22 00:00:00.000000'
+   and t.timestamp <  '2019-02-23 05:00:00.000000'
   ORDER by t.timestamp asc
 );
