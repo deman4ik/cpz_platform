52ae38adb7c9a6babe655b90ede5d9634ed7bfe9
Dmitriy Shatalov <deman4ik@gmail.com>
2019-05-28 00:56:48 +0300

    DB: robothist warn action

--- cpz-postgres-objects/functions/f_t_user_robothist_biu.sql
+++ cpz-postgres-objects/functions/f_t_user_robothist_biu.sql
@@ -19,14 +19,18 @@ begin
    elsif new.action = 'stopped_user' or new.action = 'error' then
      dDATE := null;
      nSTATUS := 20;
+   elsif new.action = 'warn' then
+    nSTATUS := null;  
    end if;
     
+  if nSTATUS is not null then
    update cpz.user_robot set
      robot_status = nSTATUS,
      last_started = (case when dDATE is not null then dDATE else last_started end)
      --run_mode = :new.run_mode -- could change on update
    where id = new.user_robot_id;
-
+  end if;
+ 
  return new;
 end;
 $$;
