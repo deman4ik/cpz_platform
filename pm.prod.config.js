d912e1afdb1051ee91af391b3618dc810182c159
Dmitriy Shatalov <deman4ik@gmail.com>
2020-04-01 01:29:31 +0300

    fix config

--- pm.prod.config.js
+++ pm.prod.config.js
@@ -3,17 +3,27 @@ module.exports = {
     {
       name: "db",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
-      args: "--env --config prod.config.js dist/services/db",
+      args: "--env --config prod.config.js --instances=3 dist/services/db",
       env: {
         NODE_ENV: "production",
         NODEID: "db"
       }
     },
     {
+      name: "backtester-runner",
+      script: "./node_modules/moleculer/bin/moleculer-runner.js",
+      args:
+        "--env --config prod.config.js dist/services/backtester/backtester-runner.service.js",
+      env: {
+        NODE_ENV: "production",
+        NODEID: "backtester-runner"
+      }
+    },
+    {
       name: "backtester-worker",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config prod.config.js dist/services/backtester/backtester-worker.service.js",
+        "--env --config prod.config.js --instances=3 dist/services/backtester/backtester-worker.service.js",
       env: {
         NODE_ENV: "production",
         NODEID: "backtester-worker"
@@ -23,7 +33,7 @@ module.exports = {
       name: "public-connector",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config prod.config.js dist/services/connector/public-connector.service.js",
+        "--env --config prod.config.js --instances=3 dist/services/connector/public-connector.service.js",
       env: {
         NODE_ENV: "production",
         NODEID: "public-connector"
@@ -43,17 +53,27 @@ module.exports = {
       name: "private-connector-worker",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env  --config prod.config.js dist/services/connector/private-connector-worker.service.js",
+        "--env  --config prod.config.js --instances=3 dist/services/connector/private-connector-worker.service.js",
       env: {
         NODE_ENV: "production",
         NODEID: "private-connector-worker"
       }
     },
     {
+      name: "exwatcher-runner",
+      script: "./node_modules/moleculer/bin/moleculer-runner.js",
+      args:
+        "--env --config prod.config.js dist/services/exwatcher/exwatcher-runner.service.js",
+      env: {
+        NODE_ENV: "production",
+        NODEID: "exwatcher-runner"
+      }
+    },
+    {
       name: "exwatcher-binance_futures",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config prod.config.js dist/services/exwatcher/binance-futures-watcher.service.js dist/services/db/candles dist/services/db/db-exwatchers.service.js",
+        "--env --config prod.config.js dist/services/db/candles dist/services/db/db-exwatchers.service.js dist/services/exwatcher/binance-futures-watcher.service.js",
       env: {
         NODE_ENV: "production",
         NODEID: "exwatcher-binance_futures"
@@ -63,7 +83,7 @@ module.exports = {
       name: "exwatcher-bitfinex",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config prod.config.js dist/services/exwatcher/bitfinex-watcher.service.js dist/services/db/candles dist/services/db/db-exwatchers.service.js",
+        "--env --config prod.config.js dist/services/db/candles dist/services/db/db-exwatchers.service.js dist/services/exwatcher/bitfinex-watcher.service.js",
       env: {
         NODE_ENV: "production",
         NODEID: "exwatcher-bitfinex"
@@ -73,17 +93,27 @@ module.exports = {
       name: "exwatcher-kraken",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config prod.config.js dist/services/exwatcher/kraken-watcher.service.js dist/services/db/candles dist/services/db/db-exwatchers.service.js",
+        "--env --config prod.config.js dist/services/db/candles dist/services/db/db-exwatchers.service.js dist/services/exwatcher/kraken-watcher.service.js",
       env: {
         NODE_ENV: "production",
         NODEID: "exwatcher-kraken"
       }
     },
     {
+      name: "importer-runner",
+      script: "./node_modules/moleculer/bin/moleculer-runner.js",
+      args:
+        "--env --config prod.config.js dist/services/importer/importer-runner.service.js",
+      env: {
+        NODE_ENV: "production",
+        NODEID: "importer-runner"
+      }
+    },
+    {
       name: "importer-worker",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config prod.config.js dist/services/importer/importer-worker.service.js",
+        "--env --config prod.config.js --instances=3 dist/services/importer/importer-worker.service.js",
       env: {
         NODE_ENV: "production",
         NODEID: "importer-worker"
@@ -103,7 +133,7 @@ module.exports = {
       name: "robot-worker",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config prod.config.js dist/services/robot/robot-worker.service.js",
+        "--env --config prod.config.js --instances=3 dist/services/robot/robot-worker.service.js",
       env: {
         NODE_ENV: "production",
         NODEID: "robot-worker"
@@ -123,17 +153,27 @@ module.exports = {
       name: "user-robot-worker",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config prod.config.js dist/services/userRobot/user-robot-worker.service.js",
+        "--env --config prod.config.js --instances=3 dist/services/userRobot/user-robot-worker.service.js",
       env: {
         NODE_ENV: "production",
         NODEID: "user-robot-worker"
       }
     },
     {
+      name: "stats-calc-runner",
+      script: "./node_modules/moleculer/bin/moleculer-runner.js",
+      args:
+        "--env --config prod.config.js dist/services/statsCalc/stats-calc-runner.service.js",
+      env: {
+        NODE_ENV: "production",
+        NODEID: "stats-calc-runner"
+      }
+    },
+    {
       name: "stats-calc-worker",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config prod.config.js dist/services/statsCalc/stats-calc-worker.service.js",
+        "--env --config prod.config.js --instances=3 dist/services/statsCalc/stats-calc-worker.service.js",
       env: {
         NODE_ENV: "production",
         NODEID: "stats-calc-worker"
@@ -153,7 +193,7 @@ module.exports = {
       name: "api",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config prod.config.js dist/services/api.service.js dist/services/auth.service.js dist/services/exwatcher/exwatcher-runner.service.js dist/services/importer/importer-runner.service.js dist/services/backtester/backtester-runner.service.js dist/services/statsCalc/stats-calc-runner.service.js",
+        "--env --config prod.config.js dist/services/api.service.js dist/services/auth.service.js",
       env: {
         NODE_ENV: "production",
         NODEID: "api"
