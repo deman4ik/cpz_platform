a1d907cd3946aa13fc8be9e74a7533f7d0dabe0e
Dmitriy Shatalov <deman4ik@gmail.com>
2020-04-20 13:01:27 +0300

    fix public connector

--- services/connector/public-connector.service.ts
+++ services/connector/public-connector.service.ts
@@ -358,49 +358,56 @@ class PublicConnectorService extends Service {
     asset: string,
     currency: string
   ): Promise<cpz.Market> {
-    await this.initConnector(exchange);
-    const response: ccxt.Market = await this.connectors[exchange].market(
-      this.getSymbol(asset, currency)
-    );
-    let loadFrom;
-    if (exchange === "kraken") {
-      const [firstTrade] = await this.getTrades(
+    try {
+      await this.initConnector(exchange);
+      const response: ccxt.Market = await this.connectors[exchange].market(
+        this.getSymbol(asset, currency)
+      );
+      let loadFrom;
+      if (exchange === "kraken") {
+        const [firstTrade] = await this.getTrades(
+          exchange,
+          asset,
+          currency,
+          dayjs.utc("01.01.2013").toISOString()
+        );
+        if (firstTrade)
+          loadFrom = dayjs
+            .utc(firstTrade.timestamp)
+            .add(1, cpz.TimeUnit.day)
+            .startOf(cpz.TimeUnit.day)
+            .toISOString();
+      } else {
+        const [firstCandle] = await this.getRawCandles(
+          exchange,
+          asset,
+          currency,
+          5,
+          dayjs.utc("01.01.2013").toISOString(),
+          10
+        );
+        if (firstCandle)
+          loadFrom = dayjs
+            .utc(firstCandle.timestamp)
+            .add(1, cpz.TimeUnit.day)
+            .startOf(cpz.TimeUnit.day)
+            .toISOString();
+      }
+      return {
         exchange,
         asset,
         currency,
-        dayjs.utc("01.01.2013").toISOString()
-      );
-      if (firstTrade)
-        loadFrom = dayjs
-          .utc(firstTrade.timestamp)
-          .add(1, cpz.TimeUnit.day)
-          .startOf(cpz.TimeUnit.day)
-          .toISOString();
-    } else {
-      const [firstCandle] = await this.getRawCandles(
-        exchange,
-        asset,
-        currency,
-        5,
-        dayjs.utc("01.01.2013").toISOString(),
-        10
-      );
-      if (firstCandle)
-        loadFrom = dayjs
-          .utc(firstCandle.timestamp)
-          .add(1, cpz.TimeUnit.day)
-          .startOf(cpz.TimeUnit.day)
-          .toISOString();
+        loadFrom,
+        limits: response.limits,
+        precision: response.precision,
+        averageFee: response.taker
+      };
+    } catch (e) {
+      if (e instanceof ccxt.ExchangeNotAvailable)
+        throw new Error("ExchangeNotAvailable");
+      if (e instanceof ccxt.NetworkError) throw new Error("NetworkError");
+      throw e;
     }
-    return {
-      exchange,
-      asset,
-      currency,
-      loadFrom,
-      limits: response.limits,
-      precision: response.precision,
-      averageFee: response.taker
-    };
   }
 
   /**
@@ -435,28 +442,35 @@ class PublicConnectorService extends Service {
     asset: string,
     currency: string
   ): Promise<cpz.ExchangePrice> {
-    await this.initConnector(exchange);
-    const call = async (bail: (e: Error) => void) => {
-      try {
-        return await this.connectors[exchange].fetchTicker(
-          this.getSymbol(asset, currency)
-        );
-      } catch (e) {
-        if (e instanceof ccxt.NetworkError) throw e;
-        bail(e);
-      }
-    };
-    const response: ccxt.Ticker = await retry(call, this.retryOptions);
-    if (!response || !response.timestamp) return null;
-    const time = dayjs.utc(response.timestamp);
-    return {
-      exchange,
-      asset,
-      currency,
-      time: time.valueOf(),
-      timestamp: time.toISOString(),
-      price: round(response.close, 6)
-    };
+    try {
+      await this.initConnector(exchange);
+      const call = async (bail: (e: Error) => void) => {
+        try {
+          return await this.connectors[exchange].fetchTicker(
+            this.getSymbol(asset, currency)
+          );
+        } catch (e) {
+          if (e instanceof ccxt.NetworkError) throw e;
+          bail(e);
+        }
+      };
+      const response: ccxt.Ticker = await retry(call, this.retryOptions);
+      if (!response || !response.timestamp) return null;
+      const time = dayjs.utc(response.timestamp);
+      return {
+        exchange,
+        asset,
+        currency,
+        time: time.valueOf(),
+        timestamp: time.toISOString(),
+        price: round(response.close, 6)
+      };
+    } catch (e) {
+      if (e instanceof ccxt.ExchangeNotAvailable)
+        throw new Error("ExchangeNotAvailable");
+      if (e instanceof ccxt.NetworkError) throw new Error("NetworkError");
+      throw e;
+    }
   }
 
   /**
@@ -475,93 +489,100 @@ class PublicConnectorService extends Service {
     currency: string,
     timeframe: cpz.Timeframe
   ): Promise<cpz.ExchangeCandle> {
-    await this.initConnector(exchange);
-    const params = getCurrentCandleParams(
-      this.connectors[exchange].timeframes,
-      timeframe
-    );
-    const call = async (bail: (e: Error) => void) => {
-      try {
-        return await this.connectors[exchange].fetchOHLCV(
-          this.getSymbol(asset, currency),
-          params.timeframeStr,
-          params.dateFrom,
-          params.limit
-        );
-      } catch (e) {
-        if (e instanceof ccxt.NetworkError) throw e;
-        bail(e);
-      }
-    };
-    const response: ccxt.OHLCV[] = await retry(call, this.retryOptions);
-    if (!response || !Array.isArray(response) || response.length === 0) {
-      const { price } = await this.getCurrentPrice(exchange, asset, currency);
-      if (!price) return null;
-      const time = dayjs.utc(params.time);
-      const roundedPrice = round(price, 6);
-      return {
-        exchange,
-        asset,
-        currency,
-        timeframe,
-        time: time.valueOf(),
-        timestamp: time.toISOString(),
-        open: roundedPrice,
-        high: roundedPrice,
-        low: roundedPrice,
-        close: roundedPrice,
-        volume: 0,
-        type: cpz.CandleType.previous
+    try {
+      await this.initConnector(exchange);
+      const params = getCurrentCandleParams(
+        this.connectors[exchange].timeframes,
+        timeframe
+      );
+      const call = async (bail: (e: Error) => void) => {
+        try {
+          return await this.connectors[exchange].fetchOHLCV(
+            this.getSymbol(asset, currency),
+            params.timeframeStr,
+            params.dateFrom,
+            params.limit
+          );
+        } catch (e) {
+          if (e instanceof ccxt.NetworkError) throw e;
+          bail(e);
+        }
       };
-    }
-    let candles: cpz.ExchangeCandle[] = response.map((candle) => {
-      try {
-        if (!candle || !Array.isArray(candle))
-          throw new Error("Wrong response");
+      const response: ccxt.OHLCV[] = await retry(call, this.retryOptions);
+      if (!response || !Array.isArray(response) || response.length === 0) {
+        const { price } = await this.getCurrentPrice(exchange, asset, currency);
+        if (!price) return null;
+        const time = dayjs.utc(params.time);
+        const roundedPrice = round(price, 6);
         return {
-          exchange,
-          asset,
-          currency,
-          timeframe: params.timeframe,
-          time: +candle[0],
-          timestamp: dayjs.utc(+candle[0]).toISOString(),
-          open: round(+candle[1], 6),
-          high: round(+candle[2], 6),
-          low: round(+candle[3], 6),
-          close: round(+candle[4], 6),
-          volume: round(+candle[5] || 0, 6),
-          type:
-            +candle[5] === 0 ? cpz.CandleType.previous : cpz.CandleType.loaded
-        };
-      } catch (e) {
-        this.logger.error(e, candle);
-        throw e;
-      }
-    });
-
-    if (candles.length > 0 && params.batch) {
-      const time = dayjs.utc(params.time);
-      candles = [
-        {
           exchange,
           asset,
           currency,
           timeframe,
           time: time.valueOf(),
           timestamp: time.toISOString(),
-          open: round(+candles[0].open, 6),
-          high: round(Math.max(...candles.map((t) => +t.high)), 6),
-          low: round(Math.min(...candles.map((t) => +t.low)), 6),
-          close: round(+candles[candles.length - 1].close, 6),
-          volume: round(
-            +candles.map((t) => t.volume).reduce((a, b) => a + b, 0) || 0,
-            6
-          ),
-          type: cpz.CandleType.created
+          open: roundedPrice,
+          high: roundedPrice,
+          low: roundedPrice,
+          close: roundedPrice,
+          volume: 0,
+          type: cpz.CandleType.previous
+        };
+      }
+      let candles: cpz.ExchangeCandle[] = response.map((candle) => {
+        try {
+          if (!candle || !Array.isArray(candle))
+            throw new Error("Wrong response");
+          return {
+            exchange,
+            asset,
+            currency,
+            timeframe: params.timeframe,
+            time: +candle[0],
+            timestamp: dayjs.utc(+candle[0]).toISOString(),
+            open: round(+candle[1], 6),
+            high: round(+candle[2], 6),
+            low: round(+candle[3], 6),
+            close: round(+candle[4], 6),
+            volume: round(+candle[5] || 0, 6),
+            type:
+              +candle[5] === 0 ? cpz.CandleType.previous : cpz.CandleType.loaded
+          };
+        } catch (e) {
+          this.logger.error(e, candle);
+          throw e;
         }
-      ];
+      });
+
+      if (candles.length > 0 && params.batch) {
+        const time = dayjs.utc(params.time);
+        candles = [
+          {
+            exchange,
+            asset,
+            currency,
+            timeframe,
+            time: time.valueOf(),
+            timestamp: time.toISOString(),
+            open: round(+candles[0].open, 6),
+            high: round(Math.max(...candles.map((t) => +t.high)), 6),
+            low: round(Math.min(...candles.map((t) => +t.low)), 6),
+            close: round(+candles[candles.length - 1].close, 6),
+            volume: round(
+              +candles.map((t) => t.volume).reduce((a, b) => a + b, 0) || 0,
+              6
+            ),
+            type: cpz.CandleType.created
+          }
+        ];
+      }
+      return candles[candles.length - 1];
+    } catch (e) {
+      if (e instanceof ccxt.ExchangeNotAvailable)
+        throw new Error("ExchangeNotAvailable");
+      if (e instanceof ccxt.NetworkError) throw new Error("NetworkError");
+      throw e;
     }
-    return candles[candles.length - 1];
   }
 
   /**
@@ -584,53 +605,60 @@ class PublicConnectorService extends Service {
     dateFrom: string,
     limit: number = 100
   ): Promise<cpz.ExchangeCandle[]> {
-    await this.initConnector(exchange);
-    const { str } = Timeframe.get(timeframe);
-    let candles: cpz.ExchangeCandle[] = [];
+    try {
+      await this.initConnector(exchange);
+      const { str } = Timeframe.get(timeframe);
+      let candles: cpz.ExchangeCandle[] = [];
 
-    const call = async (bail: (e: Error) => void) => {
-      try {
-        return await this.connectors[exchange].fetchOHLCV(
-          this.getSymbol(asset, currency),
-          str,
-          dayjs.utc(dateFrom).valueOf(),
-          limit
-        );
-      } catch (e) {
-        if (e instanceof ccxt.NetworkError) throw e;
-        bail(e);
-      }
-    };
-    const response: ccxt.OHLCV[] = await retry(call, this.retryOptions);
-    if (!response || !Array.isArray(response) || response.length === 0)
+      const call = async (bail: (e: Error) => void) => {
+        try {
+          return await this.connectors[exchange].fetchOHLCV(
+            this.getSymbol(asset, currency),
+            str,
+            dayjs.utc(dateFrom).valueOf(),
+            limit
+          );
+        } catch (e) {
+          if (e instanceof ccxt.NetworkError) throw e;
+          bail(e);
+        }
+      };
+      const response: ccxt.OHLCV[] = await retry(call, this.retryOptions);
+      if (!response || !Array.isArray(response) || response.length === 0)
+        return candles;
+
+      candles = response.map((candle) => {
+        try {
+          if (!candle || !Array.isArray(candle))
+            throw new Error("Wrong response");
+          return {
+            exchange,
+            asset,
+            currency,
+            timeframe: timeframe,
+            time: +candle[0],
+            timestamp: dayjs.utc(+candle[0]).toISOString(),
+            open: round(+candle[1], 6),
+            high: round(+candle[2], 6),
+            low: round(+candle[3], 6),
+            close: round(+candle[4], 6),
+            volume: round(+candle[5], 6) || 0,
+            type:
+              +candle[5] === 0 ? cpz.CandleType.previous : cpz.CandleType.loaded
+          };
+        } catch (e) {
+          this.logger.error(e, candle);
+          throw e;
+        }
+      });
+
       return candles;
-
-    candles = response.map((candle) => {
-      try {
-        if (!candle || !Array.isArray(candle))
-          throw new Error("Wrong response");
-        return {
-          exchange,
-          asset,
-          currency,
-          timeframe: timeframe,
-          time: +candle[0],
-          timestamp: dayjs.utc(+candle[0]).toISOString(),
-          open: round(+candle[1], 6),
-          high: round(+candle[2], 6),
-          low: round(+candle[3], 6),
-          close: round(+candle[4], 6),
-          volume: round(+candle[5], 6) || 0,
-          type:
-            +candle[5] === 0 ? cpz.CandleType.previous : cpz.CandleType.loaded
-        };
-      } catch (e) {
-        this.logger.error(e, candle);
-        throw e;
-      }
-    });
-
-    return candles;
+    } catch (e) {
+      if (e instanceof ccxt.ExchangeNotAvailable)
+        throw new Error("ExchangeNotAvailable");
+      if (e instanceof ccxt.NetworkError) throw new Error("NetworkError");
+      throw e;
+    }
   }
 
   /**
@@ -653,63 +681,70 @@ class PublicConnectorService extends Service {
     dateFrom: string,
     limit: number = 100
   ): Promise<cpz.ExchangeCandle[]> {
-    await this.initConnector(exchange);
-    const params = getCandlesParams(
-      this.connectors[exchange].timeframes,
-      timeframe,
-      dateFrom,
-      limit
-    );
-    const dateTo = dayjs.utc(params.dateTo).toISOString();
-    let candles: cpz.ExchangeCandle[] = [];
+    try {
+      await this.initConnector(exchange);
+      const params = getCandlesParams(
+        this.connectors[exchange].timeframes,
+        timeframe,
+        dateFrom,
+        limit
+      );
+      const dateTo = dayjs.utc(params.dateTo).toISOString();
+      let candles: cpz.ExchangeCandle[] = [];
 
-    const call = async (bail: (e: Error) => void) => {
-      try {
-        return await this.connectors[exchange].fetchOHLCV(
-          this.getSymbol(asset, currency),
-          params.timeframeStr,
-          params.dateFrom,
-          params.limit
-        );
-      } catch (e) {
-        if (e instanceof ccxt.NetworkError) throw e;
-        bail(e);
-      }
-    };
-    const response: ccxt.OHLCV[] = await retry(call, this.retryOptions);
-    if (!response || !Array.isArray(response) || response.length === 0)
+      const call = async (bail: (e: Error) => void) => {
+        try {
+          return await this.connectors[exchange].fetchOHLCV(
+            this.getSymbol(asset, currency),
+            params.timeframeStr,
+            params.dateFrom,
+            params.limit
+          );
+        } catch (e) {
+          if (e instanceof ccxt.NetworkError) throw e;
+          bail(e);
+        }
+      };
+      const response: ccxt.OHLCV[] = await retry(call, this.retryOptions);
+      if (!response || !Array.isArray(response) || response.length === 0)
+        return candles;
+
+      candles = response.map((candle) => {
+        try {
+          if (!candle || !Array.isArray(candle))
+            throw new Error("Wrong response");
+          return {
+            exchange,
+            asset,
+            currency,
+            timeframe: params.timeframe,
+            time: +candle[0],
+            timestamp: dayjs.utc(+candle[0]).toISOString(),
+            open: round(+candle[1], 6),
+            high: round(+candle[2], 6),
+            low: round(+candle[3], 6),
+            close: round(+candle[4], 6),
+            volume: round(+candle[5], 6) || 0,
+            type:
+              +candle[5] === 0 ? cpz.CandleType.previous : cpz.CandleType.loaded
+          };
+        } catch (e) {
+          this.logger.error(e, candle);
+          throw e;
+        }
+      });
+
+      candles = await handleCandleGaps(dateFrom, dateTo, candles);
+      if (params.batch && timeframe > cpz.Timeframe["1m"])
+        candles = await batchCandles(dateFrom, dateTo, timeframe, candles);
+
       return candles;
-
-    candles = response.map((candle) => {
-      try {
-        if (!candle || !Array.isArray(candle))
-          throw new Error("Wrong response");
-        return {
-          exchange,
-          asset,
-          currency,
-          timeframe: params.timeframe,
-          time: +candle[0],
-          timestamp: dayjs.utc(+candle[0]).toISOString(),
-          open: round(+candle[1], 6),
-          high: round(+candle[2], 6),
-          low: round(+candle[3], 6),
-          close: round(+candle[4], 6),
-          volume: round(+candle[5], 6) || 0,
-          type:
-            +candle[5] === 0 ? cpz.CandleType.previous : cpz.CandleType.loaded
-        };
-      } catch (e) {
-        this.logger.error(e, candle);
-        throw e;
-      }
-    });
-
-    candles = await handleCandleGaps(dateFrom, dateTo, candles);
-    if (params.batch && timeframe > cpz.Timeframe["1m"])
-      candles = await batchCandles(dateFrom, dateTo, timeframe, candles);
-
-    return candles;
+    } catch (e) {
+      if (e instanceof ccxt.ExchangeNotAvailable)
+        throw new Error("ExchangeNotAvailable");
+      if (e instanceof ccxt.NetworkError) throw new Error("NetworkError");
+      throw e;
+    }
   }
 
   /**
@@ -787,10 +822,10 @@ class PublicConnectorService extends Service {
 
       return trades;
     } catch (e) {
-      this.logger.error("Failed to load trades", e.message);
-      throw new Errors.MoleculerRetryableError(
-        `Failed to load trades. ${e.message}`
-      );
+      if (e instanceof ccxt.ExchangeNotAvailable)
+        throw new Error("ExchangeNotAvailable");
+      if (e instanceof ccxt.NetworkError) throw new Error("NetworkError");
+      throw e;
     }
   }
 }
