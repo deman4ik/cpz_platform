75d00ae49f02a52e8e733f4bacbcf5547f63973b
Dmitriy Shatalov <deman4ik@gmail.com>
2020-04-23 16:09:37 +0300

    config update

--- docker.config.js
+++ docker.config.js
@@ -1,14 +1,4 @@
 const brokerConfig = {
-  transporter: {
-    type: "Redis",
-    options: {
-      host: process.env.REDIS_HOST,
-      port: +process.env.REDIS_PORT,
-      password: process.env.REDIS_PASSWORD,
-      tls: process.env.REDIS_TLS && {},
-      db: 1
-    }
-  },
   logger: [
     {
       type: "Console",
--- pm.docker.config.js
+++ pm.docker.config.js
@@ -3,127 +3,85 @@ module.exports = {
     {
       name: "db",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
-      args: "--env --config docker.config.js dist/services/db",
-      env: {
-        NODEID: "db"
-      }
+      args: "--env --config docker.config.js dist/services/db"
     },
     {
       name: "backtester-worker",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config docker.config.js dist/services/backtester/backtester-worker.service.js",
-      env: {
-        NODEID: "backtester-worker"
-      }
+        "--env --config docker.config.js dist/services/backtester/backtester-worker.service.js"
     },
     {
       name: "public-connector",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config docker.config.js dist/services/connector/public-connector.service.js",
-      env: {
-        NODEID: "public-connector"
-      }
+        "--env --config docker.config.js dist/services/connector/public-connector.service.js"
     },
     {
       name: "private-connector-worker",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config docker.config.js dist/services/connector/private-connector-worker.service.js",
-      env: {
-        NODEID: "private-connector-worker"
-      }
+        "--env --config docker.config.js dist/services/connector/private-connector-worker.service.js"
     },
     {
       name: "exwatcher-binance_futures",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config docker.config.js dist/services/db/candles dist/services/exwatcher/binance-futures-watcher.service.js",
-      env: {
-        NODEID: "exwatcher-binance_futures"
-      }
+        "--env --config docker.config.js dist/services/db/candles dist/services/exwatcher/binance-futures-watcher.service.js"
     },
     {
       name: "exwatcher-bitfinex",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config docker.config.js dist/services/db/candles dist/services/exwatcher/bitfinex-watcher.service.js",
-      env: {
-        NODEID: "exwatcher-bitfinex"
-      }
+        "--env --config docker.config.js dist/services/db/candles dist/services/exwatcher/bitfinex-watcher.service.js"
     },
     {
       name: "exwatcher-kraken",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config docker.config.js dist/services/db/candles dist/services/exwatcher/kraken-watcher.service.js",
-      env: {
-        NODEID: "exwatcher-kraken"
-      }
+        "--env --config docker.config.js dist/services/db/candles dist/services/exwatcher/kraken-watcher.service.js"
     },
     {
       name: "importer-worker",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config docker.config.js dist/services/importer/importer-worker.service.js",
-      env: {
-        NODEID: "importer-worker"
-      }
+        "--env --config docker.config.js dist/services/importer/importer-worker.service.js"
     },
     {
       name: "robot-worker",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config docker.config.js dist/services/robot/robot-worker.service.js",
-      env: {
-        NODEID: "robot-worker"
-      }
+        "--env --config docker.config.js dist/services/robot/robot-worker.service.js"
     },
     {
       name: "user-robot-worker",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config docker.config.js dist/services/userRobot/user-robot-worker.service.js",
-      env: {
-        NODEID: "user-robot-worker"
-      }
+        "--env --config docker.config.js dist/services/userRobot/user-robot-worker.service.js"
     },
     {
       name: "stats-calc-worker",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config docker.config.js dist/services/statsCalc/stats-calc-worker.service.js",
-      env: {
-        NODEID: "stats-calc-worker"
-      }
+        "--env --config docker.config.js dist/services/statsCalc/stats-calc-worker.service.js"
     },
     {
       name: "publisher",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config docker.config.js dist/services/publisher.service.js dist/services/mail.service.js",
-      env: {
-        NODEID: "publisher"
-      }
+        "--env --config docker.config.js dist/services/publisher.service.js dist/services/mail.service.js"
     },
     {
       name: "api",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config api.docker.config.js dist/services/api.service.js dist/services/auth.service.js dist/services/backtester/backtester-runner.service.js dist/services/connector/private-connector-runner.service.js dist/services/exwatcher/exwatcher-runner.service.js dist/services/importer/importer-runner.service.js dist/services/robot/robot-runner.service.js dist/services/userRobot/user-robot-runner.service.js dist/services/statsCalc/stats-calc-runner.service.js",
-      env: {
-        NODEID: "api"
-      }
+        "--env --config api.docker.config.js dist/services/api.service.js dist/services/auth.service.js dist/services/backtester/backtester-runner.service.js dist/services/connector/private-connector-runner.service.js dist/services/exwatcher/exwatcher-runner.service.js dist/services/importer/importer-runner.service.js dist/services/robot/robot-runner.service.js dist/services/userRobot/user-robot-runner.service.js dist/services/statsCalc/stats-calc-runner.service.js"
     },
     {
       name: "telegram",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config docker.config.js dist/services/telegram/bot.service.js",
-      env: {
-        NODEID: "telegram"
-      }
+        "--env --config docker.config.js dist/services/telegram/bot.service.js"
     }
   ]
 };
