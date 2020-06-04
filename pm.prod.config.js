00a00f1bbca78f9c24bc4e9ecc4e31428719ab42
Dmitriy Shatalov <deman4ik@gmail.com>
2020-06-04 23:32:57 +0300

    test deploy full

--- pm.prod.config.js
+++ pm.prod.config.js
@@ -3,141 +3,211 @@ module.exports = {
     {
       name: "db",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
-      args: "--env --config prod.config.js dist/services/db",
+      args: "--env --config docker.config.js --instances=6 dist/services/db",
       env: {
+        NODE_ENV: "production",
         NODEID: "db"
-      },
-      kill_timeout: 11000
+      }
+    },
+    {
+      name: "backtester-runner",
+      script: "./node_modules/moleculer/bin/moleculer-runner.js",
+      args:
+        "--env --config docker.config.js dist/services/backtester/backtester-runner.service.js",
+      env: {
+        NODE_ENV: "production",
+        NODEID: "backtester-runner"
+      }
     },
     {
       name: "backtester-worker",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config prod.config.js dist/services/backtester/backtester-worker.service.js",
+        "--env --config docker.config.js --instances=3 dist/services/backtester/backtester-worker.service.js",
       env: {
+        NODE_ENV: "production",
         NODEID: "backtester-worker"
-      },
-      kill_timeout: 11000
+      }
     },
     {
       name: "public-connector",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config prod.config.js dist/services/connector/public-connector.service.js",
+        "--env --config docker.config.js --instances=3 dist/services/connector/public-connector.service.js",
       env: {
+        NODE_ENV: "production",
         NODEID: "public-connector"
-      },
-      kill_timeout: 11000
+      }
+    },
+    {
+      name: "private-connector-runner",
+      script: "./node_modules/moleculer/bin/moleculer-runner.js",
+      args:
+        "--env --config docker.config.js dist/services/connector/private-connector-runner.service.js",
+      env: {
+        NODE_ENV: "production",
+        NODEID: "private-connector-runner"
+      }
     },
     {
       name: "private-connector-worker",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env  --config prod.config.js dist/services/connector/private-connector-worker.service.js",
+        "--env  --config docker.config.js --instances=3 dist/services/connector/private-connector-worker.service.js",
       env: {
+        NODE_ENV: "production",
         NODEID: "private-connector-worker"
-      },
-      kill_timeout: 11000
+      }
+    },
+    {
+      name: "exwatcher-runner",
+      script: "./node_modules/moleculer/bin/moleculer-runner.js",
+      args:
+        "--env --config docker.config.js dist/services/exwatcher/exwatcher-runner.service.js",
+      env: {
+        NODE_ENV: "production",
+        NODEID: "exwatcher-runner"
+      }
     },
     {
       name: "exwatcher-binance_futures",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config prod.config.js dist/services/exwatcher/binance-futures-watcher.service.js",
+        "--env --config docker.config.js dist/services/exwatcher/binance-futures-watcher.service.js",
       env: {
+        NODE_ENV: "production",
         NODEID: "exwatcher-binance_futures"
-      },
-      kill_timeout: 11000
+      }
     },
     {
       name: "exwatcher-bitfinex",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config prod.config.js dist/services/exwatcher/bitfinex-watcher.service.js",
+        "--env --config docker.config.js dist/services/exwatcher/bitfinex-watcher.service.js",
       env: {
+        NODE_ENV: "production",
         NODEID: "exwatcher-bitfinex"
-      },
-      kill_timeout: 11000
+      }
     },
     {
       name: "exwatcher-kraken",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config prod.config.js dist/services/exwatcher/kraken-watcher.service.js",
+        "--env --config docker.config.js dist/services/exwatcher/kraken-watcher.service.js",
       env: {
+        NODE_ENV: "production",
         NODEID: "exwatcher-kraken"
-      },
-      kill_timeout: 11000
+      }
+    },
+    {
+      name: "importer-runner",
+      script: "./node_modules/moleculer/bin/moleculer-runner.js",
+      args:
+        "--env --config docker.config.js dist/services/importer/importer-runner.service.js",
+      env: {
+        NODE_ENV: "production",
+        NODEID: "importer-runner"
+      }
     },
     {
       name: "importer-worker",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config prod.config.js dist/services/importer/importer-worker.service.js",
+        "--env --config docker.config.js --instances=3 dist/services/importer/importer-worker.service.js",
       env: {
+        NODE_ENV: "production",
         NODEID: "importer-worker"
-      },
-      kill_timeout: 11000
+      }
+    },
+    {
+      name: "robot-runner",
+      script: "./node_modules/moleculer/bin/moleculer-runner.js",
+      args:
+        "--env --config docker.config.js dist/services/robot/robot-runner.service.js",
+      env: {
+        NODE_ENV: "production",
+        NODEID: "robot-runner"
+      }
     },
     {
       name: "robot-worker",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config prod.config.js dist/services/robot/robot-worker.service.js",
+        "--env --config docker.config.js --instances=3 dist/services/robot/robot-worker.service.js",
       env: {
+        NODE_ENV: "production",
         NODEID: "robot-worker"
-      },
-      kill_timeout: 11000
+      }
+    },
+    {
+      name: "user-robot-runner",
+      script: "./node_modules/moleculer/bin/moleculer-runner.js",
+      args:
+        "--env --config docker.config.js dist/services/userRobot/user-robot-runner.service.js",
+      env: {
+        NODE_ENV: "production",
+        NODEID: "user-robot-runner"
+      }
     },
     {
       name: "user-robot-worker",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config prod.config.js dist/services/userRobot/user-robot-worker.service.js",
+        "--env --config docker.config.js --instances=3 dist/services/userRobot/user-robot-worker.service.js",
       env: {
+        NODE_ENV: "production",
         NODEID: "user-robot-worker"
-      },
-      kill_timeout: 11000
+      }
+    },
+    {
+      name: "stats-calc-runner",
+      script: "./node_modules/moleculer/bin/moleculer-runner.js",
+      args:
+        "--env --config docker.config.js dist/services/statsCalc/stats-calc-runner.service.js",
+      env: {
+        NODE_ENV: "production",
+        NODEID: "stats-calc-runner"
+      }
     },
     {
       name: "stats-calc-worker",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config prod.config.js dist/services/statsCalc/stats-calc-worker.service.js",
+        "--env --config docker.config.js --instances=3 dist/services/statsCalc/stats-calc-worker.service.js",
       env: {
+        NODE_ENV: "production",
         NODEID: "stats-calc-worker"
-      },
-      kill_timeout: 11000
+      }
     },
     {
       name: "publisher",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config prod.config.js dist/services/publisher.service.js dist/services/mail.service.js",
+        "--env --config docker.config.js dist/services/publisher.service.js dist/services/mail.service.js",
       env: {
+        NODE_ENV: "production",
         NODEID: "publisher"
-      },
-      kill_timeout: 11000
+      }
     },
     {
       name: "api",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config prod.config.js dist/services/api.service.js dist/services/auth.service.js dist/services/backtester/backtester-runner.service.js dist/services/connector/private-connector-runner.service.js dist/services/exwatcher/exwatcher-runner.service.js dist/services/importer/importer-runner.service.js dist/services/robot/robot-runner.service.js dist/services/userRobot/user-robot-runner.service.js dist/services/statsCalc/stats-calc-runner.service.js",
+        "--env --config api.docker.config.js dist/services/api.service.js dist/services/auth.service.js",
       env: {
+        NODE_ENV: "production",
         NODEID: "api"
-      },
-      kill_timeout: 11000
+      }
     },
     {
       name: "telegram",
       script: "./node_modules/moleculer/bin/moleculer-runner.js",
       args:
-        "--env --config prod.config.js dist/services/telegram/bot.service.js",
+        "--env --config docker.config.js dist/services/telegram/bot.service.js",
       env: {
+        NODE_ENV: "dev",
         NODEID: "telegram"
-      },
-      kill_timeout: 11000
+      }
     }
   ]
 };
